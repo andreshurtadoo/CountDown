27e7c1050ca058f2caa533f9bda3392051b8188f
Andres Hurtado <hurtadoandres2002@gmail.com>
2023-04-09 14:40:45 -0400

    add year input

--- js/app.js
+++ js/app.js
@@ -1,16 +1,20 @@
+// Sacamos los datos de la fecha puesta por el usuario
 function send(){
     const dayValue = document.getElementsByClassName('dayValue')[0].value;
     const monthValue = document.querySelector('select').value;
-    const span = document.querySelector('span');
-
-    countdown(monthValue, dayValue)
+    const yearValue = document.getElementsByClassName('yearValue')[0].value;
+    countdown(monthValue, dayValue, yearValue)
 }
 
-const countdown = (monthValue , dayValue) => {
-    const mes = monthValue;
-    const dia = dayValue;
+// Hacemos la cuenta regresiva
+const countdown = (monthValue , dayValue, yearValue) => {
+    // Si el usuario no agrega el año, el año sera el actual
+    if (yearValue == '') yearValue = new Date().getFullYear()
 
-    const countDate = new Date(`${mes} ${dia}, 2021 00:00:00`).getTime();
+    // Sacamos el tiempo con los datos obtenidos
+    const countDate = new Date(`${monthValue} ${dayValue}, ${yearValue} 00:00:00`).getTime();
+    
+    // Calculamos el restante
     const now = new Date().getTime();
     const gap = countDate - now;
 
@@ -24,11 +28,12 @@ const countdown = (monthValue , dayValue) => {
     const textMinute = Math.floor((gap % hour) / minute);
     const textSecond = Math.floor((gap % minute) / second);
 
+    // Insertamos los valores en el HTML
     document.querySelector(".day").innerHTML = textDay;
     document.querySelector(".hour").innerHTML = textHour;
     document.querySelector(".minute").innerHTML = textMinute;
     document.querySelector(".second").innerHTML = textSecond;
-
 };
 
+// llamamos a la funcion cada 1000 milisegundos
 setInterval(countdown && send, 1000);
